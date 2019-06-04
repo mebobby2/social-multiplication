70797bb5e838c28fe5b7ce80c4004acb9a468670
Bobby Lei <bobby.lei@hreasily.com>
2019-06-04 15:09:51 +1200

    Add other domain objects

---
+++ src/main/java/numbersco/mathswiz/multiplication/domain/MultiplicationResultAttempt.java
@@ -0,0 +1,26 @@
+package numbersco.mathswiz.multiplication.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+/**
+ * MultiplicationResultAttempt
+ */
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public class MultiplicationResultAttempt {
+
+  private final User user;
+  private final Multiplication multiplication;
+  private final int resultAttempt;
+
+  MultiplicationResultAttempt() {
+    user = null;
+    multiplication = null;
+    resultAttempt = -1;
+  }
+}
---
+++ src/main/java/numbersco/mathswiz/multiplication/domain/User.java
@@ -0,0 +1,23 @@
+package numbersco.mathswiz.multiplication.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+/**
+ * User
+ */
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class User {
+  private final String alias;
+
+  // Empty constructor for JSON (de)serialization
+  protected User() {
+    alias = null;
+  }
+
+}
