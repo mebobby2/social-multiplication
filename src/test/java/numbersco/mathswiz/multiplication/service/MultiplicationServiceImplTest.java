159ed0965d003c1b91c42c75e3d67f341c88377a
Bobby Lei <bobby.lei@hreasily.com>
2019-06-04 14:40:59 +1200

    Don't use springboottest for multiplicationserviceimpltest

--- src/test/java/numbersco/mathswiz/multiplication/service/MultiplicationServiceImplTest.java
+++ src/test/java/numbersco/mathswiz/multiplication/service/MultiplicationServiceImplTest.java
@@ -3,8 +3,12 @@ package numbersco.mathswiz.multiplication.service;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -16,20 +20,23 @@ import numbersco.mathswiz.multiplication.domain.Multiplication;
 /**
  * MultiplicationServiceTest
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class MultiplicationServiceTest {
-  @MockBean
+public class MultiplicationServiceImplTest {
+  private MultiplicationServiceImpl multiplicationServiceImpl;
+
+  @Mock
   private RandomGeneratorService randomGeneratorService;
 
-  @Autowired
-  private MultiplicationService multiplicationService;
+  @Before
+public void setUp() {
+  MockitoAnnotations.initMocks(this);
+  multiplicationServiceImpl = new MultiplicationServiceImpl(randomGeneratorService);
+}
 
   @Test
   public void createRandomMultiplicationTest() {
     given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
 
-    Multiplication multiplication = multiplicationService.createRandomMultiplication();
+    Multiplication multiplication = multiplicationServiceImpl.createRandomMultiplication();
 
     assertThat(multiplication.getFactorA()).isEqualTo(50);
     assertThat(multiplication.getFactorB()).isEqualTo(30);
