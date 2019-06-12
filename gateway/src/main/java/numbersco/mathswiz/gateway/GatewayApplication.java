7f243c0aa7314824e91a30082a75d719278d121a
Bobby Lei <bobby.lei@hreasily.com>
2019-06-13 09:11:39 +1200

    Configure ribbon client to ping services and route based on availability instead of round robin

--- gateway/src/main/java/numbersco/mathswiz/gateway/GatewayApplication.java
+++ gateway/src/main/java/numbersco/mathswiz/gateway/GatewayApplication.java
@@ -3,10 +3,14 @@ package numbersco.mathswiz.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import numbersco.mathswiz.gateway.configuration.RibbonConfiguration;
 
 @EnableZuulProxy
 @EnableEurekaClient
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 @SpringBootApplication
 public class GatewayApplication {
 
