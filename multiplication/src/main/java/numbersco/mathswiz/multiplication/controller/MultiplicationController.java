ecbef1a9233216d7987f2291bb44bfc7f62b714d
Bobby Lei <bobby.lei@hreasily.com>
2019-06-13 08:37:35 +1200

    Add logging to multiplication service

--- multiplication/src/main/java/numbersco/mathswiz/multiplication/controller/MultiplicationController.java
+++ multiplication/src/main/java/numbersco/mathswiz/multiplication/controller/MultiplicationController.java
@@ -1,28 +1,35 @@
 package numbersco.mathswiz.multiplication.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 import numbersco.mathswiz.multiplication.domain.Multiplication;
 import numbersco.mathswiz.multiplication.service.MultiplicationService;
 
 /**
  * MultiplicationController
  */
+@Slf4j
 @RestController
 @RequestMapping("/multiplications")
 final class MultiplicationController {
   private final MultiplicationService multiplicationService;
 
+  private final int serverPort;
+
   @Autowired
-  public MultiplicationController(final MultiplicationService multiplicationService) {
+  public MultiplicationController(final MultiplicationService multiplicationService, @Value("${server.port}") int serverPort) {
     this.multiplicationService = multiplicationService;
+    this.serverPort = serverPort;
   }
 
   @GetMapping("/random")
   Multiplication getRandomMultiplication() {
+    log.info("Generating a random multiplication from @ {}", serverPort);
     return multiplicationService.createRandomMultiplication();
   }
 
--- multiplication/src/main/java/numbersco/mathswiz/multiplication/controller/MultiplicationResultAttemptController.java
+++ multiplication/src/main/java/numbersco/mathswiz/multiplication/controller/MultiplicationResultAttemptController.java
@@ -3,6 +3,7 @@ package numbersco.mathswiz.multiplication.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,20 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import numbersco.mathswiz.multiplication.domain.MultiplicationResultAttempt;
 import numbersco.mathswiz.multiplication.service.MultiplicationService;
 
 /**
  * MultiplicationResultAttemptController
  */
+@Slf4j
 @RestController
 @RequestMapping("/results")
 final class MultiplicationResultAttemptController {
   private final MultiplicationService multiplicationService;
+  private final int serverPort;
 
   @Autowired
-  MultiplicationResultAttemptController(final MultiplicationService multiplicationService) {
+  MultiplicationResultAttemptController(final MultiplicationService multiplicationService, @Value("${server.port}") int serverPort) {
     this.multiplicationService = multiplicationService;
+    this.serverPort = serverPort;
   }
 
   @PostMapping
@@ -50,6 +55,7 @@ final class MultiplicationResultAttemptController {
 
   @GetMapping("/{resultId}")
   ResponseEntity<MultiplicationResultAttempt> getResultById(final @PathVariable("resultId") Long resultId) {
+    log.info("Retrieving result {} from server @ {}", resultId, serverPort);
     return ResponseEntity.ok(
       multiplicationService.getResultById(resultId)
     );
