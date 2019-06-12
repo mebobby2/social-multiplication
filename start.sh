bfaaa6a45ed96934ef9266651aa87f05384b7208
Bobby Lei <bobby.lei@hreasily.com>
2019-06-12 16:53:59 +1200

    Add shell scripts for starting and stop services

---
+++ start.sh
@@ -0,0 +1,21 @@
+pushd service-registry
+nohup ./mvnw spring-boot:run &
+popd
+
+pushd gateway
+nohup ./mvnw spring-boot:run &
+popd
+
+pushd multiplication
+nohup ./mvnw spring-boot:run &
+popd
+
+pushd gamification
+nohup ./mvnw spring-boot:run &
+popd
+
+pushd ui
+nohup java -jar ~/Documents/development/jetty/start.jar &
+popd
+
+brew services start rabbitmq
