4435b03c10eb11950bf3a26c96e3df0b50066bc2
Bobby Lei <bobby.lei@hreasily.com>
2019-06-13 14:32:07 +1200

    Add circuit breaker to gamification calling multiplication

--- gamification/src/main/java/numbersco/mathswiz/gamification/client/MultiplicationResultAttemptClientImpl.java
+++ gamification/src/main/java/numbersco/mathswiz/gamification/client/MultiplicationResultAttemptClientImpl.java
@@ -1,5 +1,7 @@
 package numbersco.mathswiz.gamification.client;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -22,10 +24,15 @@ public class MultiplicationResultAttemptClientImpl implements MultiplicationResu
     this.multiplicationHost = multiplicationHost;
   }
 
+  @HystrixCommand(fallbackMethod = "defaultResult")
   @Override
   public MultiplicationResultAttempt retrieveMultiplicationResultAttemptById(final Long multiplicationResultAttemptId) {
     return restTemplate.getForObject(
       multiplicationHost + "/results/" + multiplicationResultAttemptId,
       MultiplicationResultAttempt.class);
+  }
+
+  private MultiplicationResultAttempt defaultResult(final Long multiplicationResultAttemptId) {
+    return new MultiplicationResultAttempt("fakeAlias", 10, 10, 100, true);
   }
 }
